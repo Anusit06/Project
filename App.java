23abb5ce7e20219caeaaabe8927ce679a9048ac2
Anusit06 <anusit.he@ku.th>
2025-09-24 12:21:30 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 
 public class App {
     public static void main(String[] args) {
-       new ReservationTableUI().setVisible(true);
+       new StatusGUI().setVisible(true);
     }
 }
--- USERUI/MyReservationGUI.java
+++ USERUI/MyReservationGUI.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+
 public class MyReservationGUI extends JFrame {
 
     public MyReservationGUI() {
